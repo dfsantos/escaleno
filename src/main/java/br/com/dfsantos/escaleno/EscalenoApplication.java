5142f7bfa44716e8ea97d0e9fe66939b5c449d5f
Diego Santos <diego.santos@e-unicred.com.br>
2024-02-28 00:54:30 -0300

    Inicializa projeto

---
+++ src/main/java/br/com/dfsantos/escaleno/EscalenoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dfsantos.escaleno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dfsantos/escaleno/EscalenoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dfsantos.escaleno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
